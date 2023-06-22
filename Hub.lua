0713144c183b18984741faa92a4410f89857652f
tolokdh <132098923+tolokdh@users.noreply.github.com>
2023-06-22 19:38:15 +0300

    Create Hub.lua

---
+++ Hub.lua
@@ -0,0 +1,10 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+--[[ 
+
+local Window = Library.CreateLib("ValidityTeams" , "RJTheme5")
+local Tab = Window:NewTab("Hub")
+local Section = Tab:NewSection("Script")
+Section:NewButton("Vhub" , "скрипт" , function()
+--If VHub doesn't Work On You, Please Let Me Know In Our Discord Server! --discord.gg/3NN5zTW7h2 loadstring(game:HttpGet(('https://raw.githubusercontent.com/itsyaboivincentt5315/script/main/VHub.txt'),true))()
+
